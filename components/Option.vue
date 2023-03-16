923a39c7511ec1e938786c8727e8d3a82713c5f3
Lydia Ego <egolydia97@gmail.com>
2023-03-16 11:19:19 +0100

    Feat: styled option vue component

---
+++ components/Option.vue
@@ -0,0 +1,37 @@
+    <div class="option-container">
+        <h4>{{ option.title }}</h4>
+        <div class="option-button">
+            </button>
+        </div>
+    </div>
+</template>
+<style scoped>
+.option-container {
+    margin-bottom: 2rem;
+}
+
+.option {
+    background: white;
+    outline: 0.15rem solid rgb(249, 87, 89);
+    padding: 0.75rem;
+    border: none;
+    width: 12rem;
+    font-size: 1rem;
+    color: rgb(27, 60, 138);
+    font-weight: 200;
+    cursor: pointer;
+}
+
+.option-left {
+    border-radius: 1rem 0 0 1rem;
+}
+
+.option-right {
+    border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+    background-color: rgb(249, 87, 89);
+    color: white;
+}
+</style>
