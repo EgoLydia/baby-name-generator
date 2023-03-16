2b7a7a2e0c084e49c715309b448ce78993f26f5f
Lydia Ego <egolydia97@gmail.com>
2023-03-16 11:17:44 +0100

    Feat: computed data to generate names

---
+++ data.ts
@@ -0,0 +1,79 @@
+export const names: Name[] = [
+    {
+        id: 1,
+        name:'Jane',
+        gender: Gender.GIRL,
+        popularity: Popularity.TRENDY,
+        length: Length.SHORT
+  },
+  {
+    id: 2,
+    name:'Quinn',
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.ALL
+  },
+  {
+    id: 3,
+    name:'Duncan',
+    gender: Gender.BOY,
+    popularity: Popularity.TRENDY,
+    length: Length.LONG
+  },
+  {
+    id: 4,
+    name:'Kelly',
+    gender: Gender.BOY,
+    popularity: Popularity.TRENDY,
+    length: Length.SHORT
+  },
+  {
+    id: 5,
+    name:'Cody',
+    gender: Gender.BOY,
+    popularity: Popularity.UNIQUE,
+    length: Length.ALL
+  },
+  {
+    id: 6,
+    name:'Mackenzie',
+    gender: Gender.BOY,
+    popularity: Popularity.TRENDY,
+    length: Length.LONG
+  },
+  {
+    id: 7,
+    name:'Shelby',
+    gender: Gender.GIRL,
+    popularity: Popularity.UNIQUE,
+    length: Length.ALL
+  },
+  {
+    id: 8,
+    name:'Lake',
+    gender: Gender.BOY,
+    popularity: Popularity.TRENDY,
+    length: Length.SHORT
+},
+{
+  id: 9,
+  name:'Jordan',
+  gender: Gender.BOY,
+  popularity: Popularity.UNIQUE,
+  length: Length.LONG
+  },
+  {
+    id: 10,
+    name:'Zane',
+    gender: Gender.UNISEX,
+    popularity: Popularity.TRENDY,
+    length: Length.SHORT
+  },
+  {
+    id: 11,
+    name:'Shane',
+    gender: Gender.UNISEX,
+    popularity: Popularity.UNIQUE,
+    length: Length.ALL
+},
+]
